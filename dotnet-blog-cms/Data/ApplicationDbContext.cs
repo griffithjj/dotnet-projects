42d0baa82e340bab9a9dd0abe589a81652c2a984
Jürgen Loff <jj.loff@gmail.com>
2025-08-15 13:59:21 +0200

    * Add dotnet-blog-cms project

---
+++ dotnet-blog-cms/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlogCms.Models;
+
+namespace BlogCms.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Post> Posts { get; set; } = default!;
+    }
+}
---
+++ dotnet-blog-cms/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogCms.Models;
+
+public class Post(string title = "", string summary = "", string content = "")
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Title { get; set; } = title;
+
+    [Required]
+    [StringLength(100)]
+    public string Summary { get; set; } = summary;
+
+    [StringLength(10000)]
+    public string Content { get; set; } = content;
+
+    [DataType(DataType.Date)]
+    public DateTime Created { get; set; } = DateTime.Now;
+
+    [DataType(DataType.Date)]
+    public DateTime LastUpdated { get; set; } = DateTime.Now;
+}
